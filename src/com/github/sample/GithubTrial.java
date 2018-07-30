ff4330a53ba5a52cefaf45d08269599331b5d048
josphinegp <jose.jgpr@gmail.com>
2018-07-30 18:13:54 +0800

    first commit

---
+++ src/com/github/sample/GithubTrial.java
@@ -0,0 +1,10 @@
+package com.github.sample;
+
+public class GithubTrial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("learning github");
+	}
+
+}
